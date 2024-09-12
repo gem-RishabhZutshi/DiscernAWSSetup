db781b7a754337d329d51aab692238e26d4729c8
gem-RishabhZutshi <rishabh.zutshi8@gmail.com>
2024-09-12 23:30:17 +0530

    Correcting ALB module output files

--- Airflow/modules/alb/output.tf
+++ Airflow/modules/alb/output.tf
@@ -3,21 +3,21 @@ output "alb_target_group" {
 }
 
 output "alb_listener" {
-  value = aws_alb_listener.airflow_web_server
+  value = aws_alb_listener.airflow_web_server.id
 }
 
 output "web_server_ecs_internal" {
-  value = aws_security_group.web_server_ecs_internal
+  value = aws_security_group.web_server_ecs_internal.id
 }
 
 output "scheduler_ecs_internal" {
-  value = aws_security_group.scheduler
+  value = aws_security_group.scheduler.id
 }
 
 output "workers_ecs_internal" {
-  value = aws_security_group.workers
+  value = aws_security_group.workers.id
 }
 
 output "flower_ecs_internal" {
-  value = aws_security_group.flower
+  value = aws_security_group.flower.id
 }
