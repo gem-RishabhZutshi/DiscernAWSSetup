ca9611e64b2c2257a504f8af5e461ce5ae5203d4
gem-RishabhZutshi <rishabh.zutshi8@gmail.com>
2024-10-20 03:50:08 +0530

    Turning off block public access

--- Frontend/dev/modules/S3/S3.tf
+++ Frontend/dev/modules/S3/S3.tf
@@ -7,6 +7,14 @@ resource "aws_s3_bucket" "frontend_bucket" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "frontend_bucket_public_access_block" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  block_public_acls   = false
+  block_public_policy = false
+  ignore_public_acls   = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
   bucket = aws_s3_bucket.frontend_bucket.id
   policy = jsonencode({
